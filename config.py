90fc9a2f262d02a9efa480aee3ee3956587b27f3
Sven Prüfer <pruefer.sven@gmail.com>
2018-05-01 17:16:05 +0200

    fixed typo in licence statements

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
-    DEBUG = False # Turns on debugging features in Flask
-    BCRYPT_LOG_ROUNDS = 12 # Configuration for the Flask-Bcrypt extension
-    MAIL_FROM_EMAIL = "sven@musmehl.de" # For use in application emails
-    BOOTSTRAP_SERVE_LOCAL = True # forces flask-bootstrap to use local bootstrap resources
+    DEBUG = False  # Turns on debugging features in Flask
+    BCRYPT_LOG_ROUNDS = 12  # Configuration for the Flask-Bcrypt extension
+    MAIL_FROM_EMAIL = "sven@musmehl.de"  # For use in application emails
+    BOOTSTRAP_SERVE_LOCAL = True  # forces flask-bootstrap to use local bootstrap resources
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
